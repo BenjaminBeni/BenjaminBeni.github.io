031ca2c3a9724aae8ba6617d95f50b728f10ff95
BenjaminBeni <57641039+BenjaminBeni@users.noreply.github.com>
2020-06-18 00:36:32 +0300

    Corrected the Play Again button

--- JsGame/js.js
+++ JsGame/js.js
@@ -6,70 +6,70 @@ var questions = {
         "a3": "A hammer",
         "a4": "A pistol",
         "a": "A sword",
-        "id" : 0
+        "id": 0
     },
-    "q1":{
+    "q1": {
         "q": "What is another word for lexicon?",
         "a1": "Dictionary",
         "a2": "Language",
         "a3": "Vocabulary",
         "a4": "Group of a languages words",
         "a": "Dictionary",
-        "id" : 1
+        "id": 1
     },
-    "q2":{
+    "q2": {
         "q": "What is the world's longest river?",
         "a1": "Amazon",
         "a2": "Volga",
         "a3": "Nile",
         "a4": "Rio Grande",
         "a": "Amazon",
-        "id" : 2
+        "id": 2
     },
-    "q3":{
+    "q3": {
         "q": "What is the world's largest island?",
         "a1": "Greenland",
         "a2": "Great Britain",
         "a3": "Cuba",
         "a4": "Hawai'i",
         "a": "Greenland",
-        "id" : 3
+        "id": 3
     },
-    "q4":{
+    "q4": {
         "q": "What is the diameter of Earth?",
         "a1": "8000 miles",
         "a2": "8000 kilometers",
         "a3": "8000 meters",
         "a4": "8000 kilograms",
         "a": "8000 miles",
-        "id" : 4
+        "id": 4
     },
-    "q5":{
+    "q5": {
         "q": "What is the capital of Turkeye?",
         "a1": "Ankara",
         "a2": "Istanbul",
         "a3": "Tokyo",
         "a4": "Moscow",
         "a": "Ankara",
-        "id" : 5
+        "id": 5
     },
-    "q6":{
+    "q6": {
         "q": "What is the oldest programming language?",
         "a1": "ASSEMBLY",
         "a2": "C",
         "a3": "COBOL",
         "a4": "BASIC",
         "a": "ASSEMBLY",
-        "id" : 6
+        "id": 6
     },
-    "q7":{
+    "q7": {
         "q": "Which browser has the most users?",
         "a1": "Google Chrome",
         "a2": "Safari",
         "a3": "Firefox",
         "a4": "Internet Explorer",
         "a": "Google Chrome",
-        "id" : 7
+        "id": 7
     }
 }
 
@@ -80,21 +80,22 @@ var nr = 0;
 var score = 0;
 var alist = document.getElementsByClassName("answer");
 
-function getQuestion(){
+function getQuestion() {
     var x = Math.floor(Math.random() * nrq);
-    while(used[x] != 0){
+    while (used[x] != 0) {
         x = Math.floor(Math.random() * nrq);
     }
     used[x] = 1;
     return questions["q" + x];
 }
 
-function start(){
+function start() {
 
+    document.getElementById("gl").innerText = "Good Luck!";
     document.getElementById("start").style.display = "none";
     document.getElementById("reset").style.visibility = "hidden";
 
-    for(let i = 0; i < alist.length; i++){
+    for (let i = 0; i < alist.length; i++) {
         alist[i].innerText = "";
     }
 
@@ -102,15 +103,15 @@ function start(){
     startGame(q);
 }
 
-function startGame(obj){
+function startGame(obj) {
     nr = nr + 1;
     var canswer = obj["a"];
     var qst = document.getElementById("question");
     var question = obj["q"];
-    
+
     var al = [obj["a1"], obj["a2"], obj["a3"], obj["a4"]];
     var asd = 4;
-    for(var i = 0; i < 4; i++){
+    for (var i = 0; i < 4; i++) {
         var x = Math.floor(Math.random() * asd);
         alist[i].innerText = al[x];
         al.splice(x, 1);
@@ -118,22 +119,22 @@ function startGame(obj){
     }
     qst.innerText = question;
 
-    for ( var i = 0; i < alist.length; i++ ) (function(i){
-        alist[i].onclick = function(){
-            if(alist[i].innerText == canswer){
+    for (var i = 0; i < alist.length; i++) (function (i) {
+        alist[i].onclick = function () {
+            if (alist[i].innerText == canswer) {
                 score = score + 1;
             }
-            if(nr < nrq){
+            if (nr < nrq) {
                 var q = getQuestion();
                 startGame(q);
             }
-            else{
+            else {
                 document.getElementsByTagName("h1")[0].innerText = "Congratulations, you scored " + score + " out of " + nrq;
                 nr = 0;
                 score = 0;
                 document.getElementById("reset").style.visibility = "visible";
                 document.getElementById("question").innerText = "";
-                for(let i = 0; i < alist.length; i++){
+                for (let i = 0; i < alist.length; i++) {
                     alist[i].innerText = "";
                 }
                 used = new Array(nrq).fill(0);
